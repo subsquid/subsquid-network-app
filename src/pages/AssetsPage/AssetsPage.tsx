9d57429795a976b6832f7d008092576f20de16dc
belopash <abelopashentsev@gmail.com>
2024-05-16 18:17:54 +0500

    fix: use connect wallet plug on assets page

--- src/pages/AssetsPage/AssetsPage.tsx
+++ src/pages/AssetsPage/AssetsPage.tsx
@@ -4,6 +4,7 @@ import { Box } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 
 import { CenteredPageWrapper } from '@layouts/NetworkLayout';
+import { ConnectedWalletRequired } from '@network/ConnectedWalletRequired';
 
 import { MyAssets } from './Assets';
 import { MyVestings } from './Vestings';
@@ -11,9 +12,13 @@ import { MyVestings } from './Vestings';
 export function AssetsPage() {
   return (
     <CenteredPageWrapper className="wide">
-      <MyAssets />
-      <Box sx={{ height: 64 }} />
-      <MyVestings />
+      <ConnectedWalletRequired>
+        <Box>
+          <MyAssets />
+          <Box sx={{ height: 64 }} />
+          <MyVestings />
+        </Box>
+      </ConnectedWalletRequired>
       <Outlet />
     </CenteredPageWrapper>
   );
