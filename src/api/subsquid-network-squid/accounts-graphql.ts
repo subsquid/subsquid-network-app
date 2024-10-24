81fea3327c481afc81b70a7f919c0c9f8b5b944a
belopash <abelopashentsev@gmail.com>
2024-10-24 20:51:53 +0500

    chore: remove commented code

--- src/api/subsquid-network-squid/accounts-graphql.ts
+++ src/api/subsquid-network-squid/accounts-graphql.ts
@@ -59,69 +59,6 @@ export function useMySources<TData = SourceWalletWithBalance[]>({
   };
 }
 
-// export function useMyAssets() {
-//   const datasource = useSquid();
-//   const { address } = useAccount();
-
-//   const enabled = !!address;
-//   const { data, isLoading } = useMyAssetsQuery(
-//     datasource,
-//     {
-//       address: address || '',
-//     },
-//     { enabled },
-//   );
-
-//   const assets = useMemo(() => {
-//     const accounts = data?.accounts || [];
-//     const delegations = data?.delegations || [];
-//     const workers = data?.workers || [];
-
-//     let balance = BigNumber(0);
-//     let locked = BigNumber(0);
-//     let bonded = BigNumber(0);
-//     let claimable = BigNumber(0);
-//     let delegated = BigNumber(0);
-//     const vestings: SourceWalletWithBalance[] = [];
-
-//     for (const a of accounts) {
-//       balance = balance.plus(a.balance);
-
-//       for (const o of a.owned) {
-//         locked = locked.plus(o.balance);
-//         vestings.push({
-//           id: o.id,
-//           type: AccountType.Vesting,
-//           balance: BigNumber(o.balance).toFixed(0),
-//         });
-//       }
-//     }
-//     for (const w of workers) {
-//       bonded = bonded.plus(w.bond);
-//       claimable = claimable.plus(w.claimableReward);
-//     }
-//     for (const d of delegations) {
-//       claimable = claimable.plus(d.claimableReward);
-//       delegated = delegated.plus(d.deposit);
-//     }
-
-//     return {
-//       balance: balance.toFixed(0),
-//       locked: locked.toFixed(0),
-//       bonded: bonded.toFixed(0),
-//       claimable: claimable.toFixed(0),
-//       delegated: delegated.toFixed(0),
-//       vestings,
-//       total: balance.plus(locked).plus(bonded).plus(claimable).plus(delegated).toFixed(0),
-//     };
-//   }, [data]);
-
-//   return {
-//     assets,
-//     isLoading,
-//   };
-// }
-
 export interface BlockchainApiVesting extends VestingFragmentFragment {}
 
 export class BlockchainApiVesting {
