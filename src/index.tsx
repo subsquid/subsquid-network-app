d5c44d626bebd095b9af96b99ad6eb7c9cc2fe92
belopash <abelopashentsev@gmail.com>
2025-04-21 13:04:29 +0300

    feat: remove pii from sentry reports

--- src/index.tsx
+++ src/index.tsx
@@ -65,13 +65,7 @@ if (process.env.SENTRY_DSN) {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     environment: process.env.APP_ENV,
-    integrations: [
-      Sentry.browserTracingIntegration(),
-      Sentry.replayIntegration({
-        maskAllText: false,
-        blockAllMedia: false,
-      }),
-    ],
+    integrations: [Sentry.browserTracingIntegration()],
     ignoreErrors: [
       // Random plugins/extensions
       'top.GLOBALS',
@@ -95,13 +89,8 @@ if (process.env.SENTRY_DSN) {
       /^chrome-extension:\/\//i,
       /^moz-extension:\/\//i,
     ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-    // tracePropagationTargets: [/^https:\/\/yourserver\.io\/api/],
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    tracesSampleRate: 1.0,
+    sendDefaultPii: false,
   });
 }
 
